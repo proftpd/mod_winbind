86d58bfc55e38cb46512c300a902968e8a13a3fc
John Morrissey <jwm@horde.net>
2012-09-25 13:37:13 -0700

    avoid spurious error when looking up UIDs not in winbindd's idmap range
    i'm not 100% sure that's what is causing the problem, but it seems fairly
    innocuous to suppress this error only for getpwuid lookups
    
    https://forums.proftpd.org/smf/index.php/topic,5630.0/all.html

--- mod_winbind.c
+++ mod_winbind.c
@@ -313,7 +313,7 @@ handle_winbind_uid_name(cmd_rec *cmd)
 
   ret = wbcGetpwuid(*((uid_t *) cmd->argv[0]), &pw);
   if (!WBC_ERROR_IS_OK(ret)) {
-    if (ret != WBC_ERR_UNKNOWN_USER) {
+    if ((ret != WBC_ERR_UNKNOWN_USER) && (ret != WBC_ERR_DOMAIN_NOT_FOUND)) {
       pr_log_pri(PR_LOG_ERR,
         MOD_WINBIND_VERSION ": unable to look up user with UID %u: %s",
         *((unsigned *) cmd->argv[0]), wbcErrorString(ret));
